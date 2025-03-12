66b3a70ad80396e9981b02eb3ee057d1dc24447b
Adoms <adoms@yandex.ru>
2025-03-13 07:53:58 +0900

    Backup данных блога

--- base/settings.py
+++ base/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "blog.apps.BlogConfig",
-    "main",
+    "main.apps.MainConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
