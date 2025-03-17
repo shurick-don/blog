d7059ee45ac3dfc011d06e381784f773d05a3f7e
Adoms <adoms@yandex.ru>
2025-03-17 15:52:02 +0900

    Удаление лишних файлов

--- main/urls.py
+++ main/urls.py
@@ -6,8 +6,4 @@ app_name = "main"
 
 urlpatterns = [
     path("", index, name="general"),
-<<<<<<< HEAD
-    # path("pint/", pint, name="pint"),
-=======
->>>>>>> b0be9c4 (Перенос шаблонов gallery в приложение blog)
 ]
--- main/views.py
+++ main/views.py
@@ -5,11 +5,3 @@ from blog.models import Post
 
 def index(request):
     return render(request, "main/index.html")
-<<<<<<< HEAD
-
-
-# def pint(request):
-#     posts = Post.objects.all()
-#     return render(request, "main/pinlist.html", {"posts": posts})
-=======
->>>>>>> b0be9c4 (Перенос шаблонов gallery в приложение blog)
